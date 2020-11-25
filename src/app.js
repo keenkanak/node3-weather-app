a301df55fca1867ebdb40b8f9957a6a5e0ddf3fb
Kanak Dullu <kanak101.kd@gmail.com>
2020-11-25 15:40:16 +0530

    refactor

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const geocode = require('./utils/geocode.js')
 const weatherStack = require('./utils/weatherStack.js')
 const path = require('path')
 const express = require('express')
-const hbs = require('hbs');
+const hbs = require('hbs')
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -23,6 +23,7 @@ app.get('', (req, res) => {
         title: "Weather App",
         name: "Kanak Dullu"
     })
+
 })
 app.get('/about', (req, res) => {
     res.render('about', {
@@ -38,67 +39,67 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
-    if(!req.query.address){
+    if (!req.query.address) {
         return res.send({
-            error:'You must provide an address'
+            error: 'You must provide an address'
         })
-    }
-    
-    console.log(req.query.address)
-    const address = req.query.address
-    
-geocode(address, (error, {Latitude,Longitude,Location} = {}) => {
-    if(error){
-       return res.send(error)
     }
 
-    weatherStack(Latitude,Longitude, (error, forecastData) => {
-        if(error){
+    console.log(req.query.address)
+    const address = req.query.address
+
+    geocode(address, (error, { Latitude, Longitude, Location } = {}) => {
+        if (error) {
             return res.send(error)
         }
-        res.send( {
-            Latitude:Latitude,
-            Longitude:Longitude,
-            Location: Location, 
-            Forecast:forecastData
+
+        weatherStack(Latitude, Longitude, (error, forecastData) => {
+            if (error) {
+                return res.send(error)
+            }
+            res.send({
+                Latitude: Latitude,
+                Longitude: Longitude,
+                Location: Location,
+                Forecast: forecastData
+            })
         })
+
     })
-    
-})
 
     // res.send({
     //     Location:address,
     //     Temperature:32
     // })
-}) 
+})
 //req holds the query string among other things
-app.get('/products', (req, res) => {
-    if(!req.query.search){
-        return res.send({
-            error:'You must provide a search'
-        })
-    }
-    console.log(req.query)
-    res.send({
-        products: []
+// app.get('/products', (req, res) => {
+//     if (!req.query.search) {
+//         return res.send({
+//             error: 'You must provide a search'
+//         })
+//     }
+//     console.log(req.query)
+//     res.send({
+//         products: []
+//     })
+// })
+app.get('/help/*', (req, res) => {
+    res.render("404", {
+        title: "404",
+        name: 'Kanak Dullu',
+        errorMessage: "Help page not found"
     })
-}) 
-app.get('/help/*',(req,res) =>{
-    res.render("404",{
-        title:"404",
-        name:'Kanak Dullu',
-        errorMessage : "Help page not found"
-    })
-})    
+})
 
 
-app.get('*',(req,res) =>{
-    res.render("404",{
-        title:"404",
-        name:'Kanak',
-        errorMessage : "Page not found"
+app.get('*', (req, res) => {
+    res.render("404", {
+        title: "404",
+        name: 'Kanak',
+        errorMessage: "Page not found"
     })
-})    
+})
 
 
 app.listen(port, () => {
